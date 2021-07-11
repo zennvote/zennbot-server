d10126fe1f24f4099132ed3fef09f33ad2c1435a
XxshiftxX <contact@dev-shift.me>
2021-07-12 00:08:12 +0900

    fix: cooltime 기능 재활성화

--- src/commands/viewers.command.ts
+++ src/commands/viewers.command.ts
@@ -51,7 +51,7 @@ export const requestSong = async (payload: ChatEvent) => {
     throw new Error('No tag on chat: username | display-name');
   }
 
-  if (await songModel.isCooltime(requestor) && false) {
+  if (await songModel.isCooltime(requestor)) {
     sendMessage(payload.channel, '아직 곡을 신청할 수 없어요! 이전에 신청한 곡 이후로 최소 4개의 곡이 신청되어야 해요.');
     return;
   }
