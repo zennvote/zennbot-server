317b70c96087d08160833d59ee2f06d521df6727
XxshiftxX <contact@dev-shift.me>
2021-07-12 18:53:01 +0900

    fix: lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,19 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
-    "airbnb-base",
-    "plugin:@typescript-eslint/eslint-recommended"
-  ]
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+      },
+    },
+  },
 };
