b4c5505440c9560f64c6043b9ca00d8214224a37
XxshiftxX <contact@dev-shift.me>
2021-08-02 03:27:22 +0900

    fix: redis 연결 시 config uri 사용하도록 변경

--- src/infrastructure/redis.ts
+++ src/infrastructure/redis.ts
@@ -1,6 +1,7 @@
 import Redis from 'redis';
+import { config } from '../utils/config';
 
-export const redis = Redis.createClient();
+export const redis = Redis.createClient(config.redis.uri ?? '');
 
 redis.set('songs/removed-list', '[]');
 redis.set('songs/list', '[]');
