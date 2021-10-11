d474ca76220a70be91974bbc05911ee033ab4339
XxshiftxX <contact@dev-shift.me>
2021-10-11 23:52:43 +0900

    enqueueSong, deleteSong 스펙 작성

--- src/services/songs.spec.ts
+++ src/services/songs.spec.ts
@@ -35,4 +35,66 @@ describe('songs.service.ts', () => {
       songs.should.deep.equals(expected);
     });
   });
+
+  describe('함수 enqueueSong', () => {
+    it('새 신청곡을 Queue의 맨 끝에 넣어야 한다', async () => {
+      // Arrange
+      const getSongListMock = sandbox.stub(SongModel, 'getSongList');
+      const setSongListMock = sandbox.stub(SongModel, 'setSongList');
+      const expected = [getSongFixture(), getSongFixture(), getSongFixture()];
+      getSongListMock.resolves([expected[0], expected[1]]);
+
+      // Act
+      await SongService.enqueueSong(expected[2]);
+
+      // Assert
+      setSongListMock.firstCall.args[0].should.deep.equal(expected);
+    });
+  });
+
+  describe('함수 deleteSong', () => {
+    it('전달받은 index에 해당하는 신청곡을 삭제하고 true를 반환한다.', async () => {
+      // Arrange
+      const getSongListMock = sandbox.stub(SongModel, 'getSongList');
+      const setSongListMock = sandbox.stub(SongModel, 'setSongList');
+      const existing = [getSongFixture(), getSongFixture(), getSongFixture()];
+      const expected = [existing[0], existing[2]];
+      getSongListMock.resolves(existing);
+
+      // Act
+      const result = await SongService.deleteSong(1);
+
+      // Assert
+      result.should.be.true;
+      setSongListMock.firstCall.args[0].should.deep.equal(expected);
+    });
+
+    it('전달받은 index가 음수일 경우 false를 반환한다.', async () => {
+      // Arrange
+      const getSongListMock = sandbox.stub(SongModel, 'getSongList');
+      const setSongListMock = sandbox.stub(SongModel, 'setSongList');
+
+      // Act
+      const result = await SongService.deleteSong(-1);
+
+      // Assert
+      result.should.be.false;
+      getSongListMock.called.should.be.false;
+      setSongListMock.called.should.be.false;
+    });
+
+    it('전달받은 index에 해당하는 신청곡이 없는 경우 false를 반환한다.', async () => {
+      // Arrange
+      const getSongListMock = sandbox.stub(SongModel, 'getSongList');
+      const setSongListMock = sandbox.stub(SongModel, 'setSongList');
+      getSongListMock.resolves([getSongFixture(), getSongFixture()]);
+
+      // Act
+      const result = await SongService.deleteSong(2);
+
+      // Assert
+      result.should.be.false;
+      setSongListMock.called.should.be.false;
+    });
+  });
 });
