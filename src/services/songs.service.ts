5de9f8692835812372b8093833a66e85a925c18e
XxshiftxX <contact@dev-shift.me>
2021-10-11 23:52:01 +0900

    deleteSong의 반환 타입 변경

--- src/services/songs.service.ts
+++ src/services/songs.service.ts
@@ -2,9 +2,13 @@ import Song, * as SongsModel from '../models/songs.model';
 
 export const getSongs = (): Promise<Song[]> => SongsModel.getSongList();
 
-export const enqueueSong = async (song: Song): Promise<void> => {};
+export const enqueueSong = async (song: Song): Promise<void> => {
+  const existing = await SongsModel.getSongList();
 
-export const deleteSong = async (index = 0): Promise<void> => {};
+  SongsModel.setSongList([...existing, song]);
+};
+
+export const deleteSong = async (index = 0): Promise<boolean> => true;
 
 export const isCooltime = async (username: string): Promise<boolean> => true;
 
