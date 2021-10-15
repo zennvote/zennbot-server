f1bc3c9381a41cd98e1dfa4b908f85690d08aa50
XxshiftxX <contact@dev-shift.me>
2021-10-15 20:01:15 +0900

    version check api 추가

--- src/infrastructure/app.ts
+++ src/infrastructure/app.ts
@@ -1,6 +1,8 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 
+import { execSync } from 'child_process';
+
 import SongsRouter from '../controllers/songs.controller';
 import FlagsRouter from '../controllers/flags.controller';
 
@@ -9,6 +11,12 @@ export const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/version', (req, res) => {
+  const result = execSync('git rev-parse HEAD').toString().trim();
+
+  res.end(result);
+});
 
 app.use('/api/songs', SongsRouter);
 app.use('/api/flags', FlagsRouter);
