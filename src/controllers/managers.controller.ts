15051f92e2d1d68f5ecc250f3523ac61b78af498
XxshiftxX <contact@dev-shift.me>
2022-02-10 01:46:28 +0900

    /api/managers 컨트롤러 구현

--- src/controllers/managers.controller.ts
+++ src/controllers/managers.controller.ts
@@ -9,4 +9,34 @@ router.get('/', async (req, res) => {
   res.json(managers);
 });
 
+router.post('/:username', async (req, res) => {
+  const result = await business.postManager(req.params.username);
+
+  if (result.isErr()) {
+    const { error } = result;
+
+    if (error === business.PostManagerError.ManagerAlreadyExists) {
+      return res.status(400).json({ error: 'ManagerAlreadyExists' });
+    }
+    return res.status(500);
+  }
+
+  return res.status(200).json(result.value);
+});
+
+router.delete('/:username', async (req, res) => {
+  const result = await business.deleteManager(req.params.username);
+
+  if (result.isErr()) {
+    const { error } = result;
+
+    if (error === business.DeleteManagerError.ManagerNotExists) {
+      return res.status(404).json({ error: 'ManagerNotExists' });
+    }
+    return res.status(500);
+  }
+
+  return res.status(200).json(result.value);
+});
+
 export default router;
--- src/controllers/managers.spec.ts
+++ src/controllers/managers.spec.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import sinon from 'sinon';
+import { err, ok } from 'neverthrow';
 
 import { app } from '../infrastructure/app';
 import { getManagerFixture } from '../models/managers.fixture';
@@ -17,7 +18,7 @@ describe('managers.controller.ts', () => {
     sinon.restore();
   });
 
-  describe('/api/managers', () => {
+  describe('GET /api/managers', () => {
     it('매니저 목록을 조회할 수 있어야 한다.', async () => {
       // Arrange
       const getManagersMock = sandbox.stub(ManagersBusiness, 'getManagers');
@@ -28,7 +29,40 @@ describe('managers.controller.ts', () => {
       const response = await request(app).get('/api/managers');
 
       // Assert
+      response.statusCode.should.be.equal(200);
       response.body.should.deep.equal(expected);
+    });
+  });
+
+  describe('POST /api/managers/:username', () => {
+    it('매니저를 생성할 수 있어야 한다.', async () => {
+      // Arrange
+      const postManagerMock = sandbox.stub(ManagersBusiness, 'postManager');
+      const expected = getManagerFixture();
+      postManagerMock.resolves(ok(expected));
+
+      // Act
+      const response = await request(app).post(`/api/managers/${encodeURI(expected.username)}`);
+
+      // Assert
+      response.statusCode.should.be.equal(200);
+      response.body.should.deep.equal(expected);
+      postManagerMock.calledWith(expected.username).should.be.true;
+    });
+
+    it('매니저가 이미 존재하면 생성되지 않아야 한다', async () => {
+      // Arrange
+      const postManagerMock = sandbox.stub(ManagersBusiness, 'postManager');
+      const expected = getManagerFixture();
+      postManagerMock.resolves(err(ManagersBusiness.PostManagerError.ManagerAlreadyExists));
+
+      // Act
+      const response = await request(app).post(`/api/managers/${encodeURI(expected.username)}`);
+
+      // Assert
+      response.statusCode.should.be.equal(400);
+      response.body.should.deep.equal({ error: 'ManagerAlreadyExists' });
+      postManagerMock.calledWith(expected.username).should.be.true;
     });
   });
 });
